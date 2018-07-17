06ebec0d91ec8e73512a1125d71164c761a611ba
Tim Oliver <me@timoliver.com.au>
2018-07-18 01:05:20 +0900

    Added initial console logic

--- PhotoSorter/main.swift
+++ PhotoSorter/main.swift
@@ -8,12 +8,29 @@
 
 import Foundation
 import Commander
+import ImageIO
+
+// MARK: - Sort -
+
+func sortPhotos(folders: [String], output: String) {
+    
+}
+
+// MARK: - Main -
 
 command(
-    Option("name", default: "world"),
-    Option("count", default: 1, description: "The number of times to print.")
-) { name, count in
-    for _ in 0..<count {
-        print("Hello \(name)")
+    VariadicOption<String>("folder", description: "File path to folder to scan for photos"),
+    Option<String>("output", default: "~/Desktop/Photos-Sorted", description: "File path to output folder (Will be created if doesn't exist")
+) { folders, output in
+    guard folders.count > 0 else {
+        print("Please specify one or more folders to scan.")
+        return
     }
+    
+    guard !output.isEmpty else {
+        print("Please specify a destination folder.")
+        return
+    }
+    
+    sortPhotos(folders: folders, output: output)
 }.run()
